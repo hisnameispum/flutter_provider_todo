9a4bf60359771f867b28c8fd88648481f1fb0c47
hisnameispum <sirasitxp@gmail.com>
2021-01-28 00:54:08 -0800

    Finished the top part

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_provider_todo/main.dart';
+
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
-      body: Container(
-        padding: EdgeInsets.only(left: 20, top: 20),
-        child: Column(
-          children: [
-            Icon(Icons.view_stream_outlined)
-          ],
-        ),
+      backgroundColor: Colors.white,
+      body: Wrap(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.yellow,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+              ),
+            ),
+            padding: EdgeInsets.only(left: 20, top: 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+                Icon(FontAwesome5Solid.stream),
+                SizedBox(
+                  height: 10,
+                  width: double.infinity,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 60,bottom: 30),
+                  child: Text(
+                    'Tasks',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
